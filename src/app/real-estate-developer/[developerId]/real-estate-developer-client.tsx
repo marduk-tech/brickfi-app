62d240e253edf233f2ffb3f1214234ca74516d20
infinity <aseemagarwal19@gmail.com>
2025-09-25 12:45:45 +0530

    pushing builder page changes

--- src/app/real-estate-developer/[developerId]/real-estate-developer-client.tsx
+++ src/app/real-estate-developer/[developerId]/real-estate-developer-client.tsx
@@ -10,6 +10,9 @@ import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import RealEstateDeveloperLoading from "./loading";
+import LandingHeader from "@/custom-pages/landing/header";
+import LandingFooter from "@/custom-pages/landing/footer";
+import { capitalize } from "@/libs/lvnzy-helper";
 
 const { Paragraph } = Typography;
 
@@ -111,21 +114,146 @@ export default function RealEstateDeveloperClient({
       description: "Please try again later",
     });
   }
+  const renderProject = (project: any, index: number) => {
+    return (
+      <Flex
+        key={index}
+        style={{
+          width: isMobile ? "100%" : 200,
+          border: `1.5px solid ${COLORS.borderColor}`,
+          borderRadius: 8,
+          padding: 4,
+        }}
+        vertical
+      >
+        <div
+          style={{
+            backgroundImage: `url(/images/builder-page/${
+              project.type == "residential" ? project.subType : "commercial"
+            }.png)`,
+            backgroundPosition: "center",
+            backgroundSize: "60%",
+            backgroundRepeat: "no-repeat",
+            height: 100,
+            width: "100%",
+          }}
+        ></div>
+        <Flex vertical style={{ marginTop: 8 }}>
+          <Typography.Text
+            style={{
+              fontSize: FONT_SIZE.HEADING_2,
+              fontWeight: 500,
+              lineHeight: "100%",
+              marginBottom: 8,
+            }}
+          >
+            {project.name}
+          </Typography.Text>
+          <Typography.Text
+            style={{ textWrap: "wrap", fontSize: FONT_SIZE.PARA }}
+          >
+            {capitalize(project.type)} | {capitalize(project.subType)}
+          </Typography.Text>
+          <Typography.Text
+            style={{
+              textWrap: "wrap",
+              fontSize: FONT_SIZE.PARA,
+              color: COLORS.textColorMedium,
+            }}
+          >
+            {project.location}
+          </Typography.Text>
+        </Flex>
+      </Flex>
+    );
+  };
 
   return (
-    <Flex vertical style={{ maxWidth: MAX_WIDTH, margin: "auto" }}>
-      <Flex vertical style={{ padding: 16 }}>
-        <Typography.Text
-          style={{ fontSize: FONT_SIZE.HEADING_1, fontWeight: "bold" }}
-        >
-          {developer.name}
+    <>
+      <LandingHeader
+        bgColor="white"
+        logo={"/images/brickfi-logo.png"}
+        color={COLORS.textColorDark}
+      ></LandingHeader>
+      <Flex
+        vertical
+        style={{
+          maxWidth: MAX_WIDTH,
+          margin: "auto",
+          paddingTop: 100,
+          paddingBottom: 100,
+        }}
+      >
+        <Typography.Text style={{ color: COLORS.textColorMedium }}>
+          Real Estate Developer &gt; {developer.name}
         </Typography.Text>
-        <Paragraph ellipsis={{ rows: 2, expandable: true }}>
-          {developer.info?.oneLiner}
-        </Paragraph>
+        <Flex vertical>
+          <Typography.Text
+            style={{ fontSize: FONT_SIZE.HEADING_1, fontWeight: "bold" }}
+          >
+            {developer.name}
+          </Typography.Text>
+          <Paragraph
+            style={{ marginBottom: 32 }}
+            ellipsis={{ rows: 6, expandable: true }}
+          >
+            {developer.info?.oneLiner}
+          </Paragraph>
 
-        <Tabs defaultActiveKey="projects" items={items} />
+          {/* <Tabs defaultActiveKey="projects" items={items} /> */}
+          <Flex style={{ width: "100%", flexWrap: "wrap" }} gap={16}>
+            {developer.genDetails.details.projects
+              ?.slice(
+                0,
+                10
+              )
+              .map((project: any, index: number) => {
+                return renderProject(project, index);
+              })}
+
+            {/* <Flex
+              key={developer.genDetails.details.projects.length}
+              style={{
+                width: isMobile ? "100%" : 200,
+                border: `1.5px solid ${COLORS.borderColor}`,
+                borderRadius: 8,
+                padding: 4,
+                
+              }}
+              vertical
+            >
+              <div
+                style={{
+                  backgroundImage: `url(/images/builder-page/lock-symbol.png)`,
+                  backgroundPosition: "center",
+                  backgroundSize: "30%",
+                  backgroundRepeat: "no-repeat",
+                  height: "100%",
+                  width: "100%",
+                  opacity: 0.4
+                }}
+              ></div>
+            </Flex> */}
+          </Flex>
+
+          <Flex
+            align="center"
+            style={{ width: "100%", marginTop: 32 }}
+            justify="center"
+          >
+            <img
+              style={{
+                border: `1px solid ${COLORS.borderColor}`,
+                borderRadius: 8,
+              }}
+              src="/images/builder-page/free-report-cta.png"
+              height={300}
+              width="auto"
+            ></img>
+          </Flex>
+        </Flex>
       </Flex>
-    </Flex>
+      <LandingFooter></LandingFooter>
+    </>
   );
 }
