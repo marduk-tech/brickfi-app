a33418e756228dcc26b9651a885070ef2838b319
infinity <aseemagarwal19@gmail.com>
2025-09-30 14:35:09 +0530

    fixing meta tags

--- src/app/brickassist/page.tsx
+++ src/app/brickassist/page.tsx
@@ -2,10 +2,13 @@ import BrickAssistLanding from "@/custom-pages/landing/brick-assist-landing";
 import { Metadata } from "next";
 
 
+
+const META_DESCR = "Consult with Brickfi to get an expert advice on your next home purchase. We provide unbiased, data backed and technology driven real estate advisory.";
+const META_TITLE = "Brickfi | Unbiased & Data-Backed Home Buying Advise.";
 export const metadata: Metadata = {
-  title: "Brickfi | Explore unbiased and data backed home buying advise.",
+  title: META_TITLE,
   description:
-    "Consult with Brickfi to get an expert advice on your next home purchase. We provide unbiased, data backed and technology driven real estate advisory.",
+  META_DESCR,
   keywords: [
     "real estate",
     "property",
@@ -27,9 +30,9 @@ export const metadata: Metadata = {
     canonical: "/",
   },
   openGraph: {
-    title: "Brickfi | Explore unbiased and data backed home buying advise.",
+    title: META_TITLE,
     description:
-      "Consult with Brickfi to get an expert advice on your next home purchase. We provide unbiased, data backed and technology driven real estate advisory.",
+      META_DESCR,
     url: "https://brickfi.in",
     siteName: "Brickfi",
     images: [
@@ -37,7 +40,7 @@ export const metadata: Metadata = {
         url: "/images/brickassist-preview.png",
         width: 1200,
         height: 630,
-        alt: "Brickfi | Explore unbiased and data backed home buying advise.",
+        alt: META_TITLE,
       },
     ],
     locale: "en_IN",
@@ -45,9 +48,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Brickfi | Explore unbiased and data backed home buying advise.",
+    title: META_TITLE,
     description:
-      "Consult with Brickfi to get an expert advice on your next home purchase. We provide unbiased, data backed and technology driven real estate advisory.",
+      META_DESCR,
     images: ["/images/brickassist-preview.png"],
   },
   robots: {
