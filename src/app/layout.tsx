0016befe69f3bb2b65ecbe2553b7b12ff44ba331
infinity <aseemagarwal19@gmail.com>
2025-10-13 14:16:24 +0530

    adding google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClientProviders } from "../components/client-providers";
 import AntdRegistry from "../components/antd-registry";
 import "../theme/globals.scss";
 import PosthogProvider from "@/components/common/posthog-provider";
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://brickfi.in"),
@@ -83,6 +84,24 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+         <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q2HFD85Y22"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q2HFD85Y22');
+            `,
+          }}
+        />
+      </head>
       <body>
         <AntdRegistry>
           <ClientProviders>{children}</ClientProviders>
