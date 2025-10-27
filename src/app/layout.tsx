04e566414069f9d63e2da354caf4cfb844af7d3b
SahilMahadwar <accounts@mahadwar.com>
2025-10-27 19:37:02 +0530

    debug statusbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import PosthogProvider from "@/components/common/posthog-provider";
-import CapacitorStatusBar from "@/components/common/capacitor-status-bar";
+
 import type { Metadata } from "next";
 import Script from "next/script";
 import AntdRegistry from "../components/antd-registry";
@@ -163,7 +163,6 @@ fbq('track', 'PageView');
           />
         </noscript>
 
-        <CapacitorStatusBar />
         <AntdRegistry>
           <ClientProviders>{children}</ClientProviders>
         </AntdRegistry>
