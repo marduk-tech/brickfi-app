d66c052eda857f63f350d5f6b8e9c63116fbfd2c
infinity <aseemagarwal19@gmail.com>
2026-03-06 10:27:38 +0530

    pushing media tab change

--- src/components/brick-360/media-tab.tsx
+++ src/components/brick-360/media-tab.tsx
@@ -6,10 +6,36 @@ interface MediaTabProps {
 }
 
 export const MediaTab = ({ lvnzyProject }: MediaTabProps) => {
+
+  const processMedia = () => {
+    const media: any[] = [];
+    lvnzyProject?.originalProjectId.media.forEach((m: any) => {
+      if (!!m.image.tags.length && !m.image.tags.includes("floorplan") && !m.image.tags.includes("na")) {
+         m.image.caption = "";
+        media.push(m);
+      }
+    })
+    lvnzyProject.originalProjectId.info.unitConfigWithPricing.forEach((u: any) => {
+      if (u.floorplans && u.floorplans.length) {
+        u.floorplans.forEach((f: any) => {
+          media.push({
+            type: "image",  
+            image: {
+              tags: ["floorplan"],
+              url: f,
+              caption: `${u.type}/ SBA: ${u.sizeBuiltup}`
+            }
+          })
+        })
+      }
+    })
+    return media;
+  }
+ 
   return (
     <ScrollableContainer>
       <ProjectGalleryV2
-        media={lvnzyProject?.originalProjectId.media}
+        media={processMedia()}
         selectedImageId={null}
       />
     </ScrollableContainer>
