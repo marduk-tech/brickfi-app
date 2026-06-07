a694fbaa9f1a35a720c16f9bcac0a40b4f63b2be
infinity <aseemagarwal19@gmail.com>
2026-06-07 14:59:37 +0530

    fixin build error

--- src/components/map-view-v2/brick-map/brick-map-customer.tsx
+++ src/components/map-view-v2/brick-map/brick-map-customer.tsx
@@ -11,7 +11,7 @@ import type { ProjectMarkerInput } from "../map-view-v2";
 import { COLORS, FONT_SIZE } from "@/theme/style-constants";
 import moment from "moment";
 import { capitalize, getMinMaxPrices } from "@/libs/lvnzy-helper";
-import MapViewWrapper from "../map-view-google/map-view-wrapper";
+const MapViewWrapper = dynamic(() => import("../map-view-google/map-view-wrapper"), { ssr: false });
 const MapViewV2 = dynamic(() => import("../map-view-v2"), { ssr: false });
 
 export function BrickMapCustomer({
